0e213167bf0070ca751a1a727e1576a990b3a723
admin <admin@LAPTOP-7KRDQTII>
2020-10-24 20:03:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorMassiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            String FileName;
+
+            Console.Write("Inter N > ");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Inter file name (without ext) > ");
+            FileName = Console.ReadLine();
+            FileName += ".txt";
+
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(FileName);
+            Console.SetOut(new_out);
+
+            Console.WriteLine(N);
+
+            Random r = new Random(DateTime.Now.Millisecond);
+            int x = 0;
+            for (int i = 0; i < N; i++)
+            {
+                x = r.Next(1000);
+                Console.Write(x + " ");
+
+            }
+
+            Console.SetOut(save_out); new_out.Close();
+            Console.WriteLine("File " + FileName + " was created!!!!");
+            Console.ReadKey();
+        }
+    }
+}
